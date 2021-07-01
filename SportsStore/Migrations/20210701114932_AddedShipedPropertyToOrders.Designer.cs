a18d814e0ee8420701041c651b042e0d5d2e8a41
Buk7op <robxviktor@gmail.com>
2021-07-01 16:10:06 +0300

    Finished order managment

--- SportsStore/Migrations/20210701114932_AddedShipedPropertyToOrders.Designer.cs
+++ SportsStore/Migrations/20210701114932_AddedShipedPropertyToOrders.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsStore.Data;
 
 namespace SportsStore.Migrations
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210701114932_AddedShipedPropertyToOrders")]
+    partial class AddedShipedPropertyToOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

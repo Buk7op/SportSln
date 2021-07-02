a05d8890af46071d0ea17a5e7734941f2a6389e9
Buk7op <robxviktor@gmail.com>
2021-07-02 13:43:28 +0300

    Added Products managment panel

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,10 +10,15 @@ namespace SportsStore.Models
     public class Product
     {
         public long ProductID { get; set; }
+        [Required(ErrorMessage = "Please enter a name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a Description")]
         public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         [Column(TypeName = "decimal(8,2)")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Please enter a category")]
         public string Category { get; set; }
     }
 }
